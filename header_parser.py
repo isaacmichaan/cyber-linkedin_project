ddc44de7e78df618f758a19a2ef414b8d3122fba
Isaac Michaan <46087877+isaacmichaan@users.noreply.github.com>
2020-03-17 16:40:09 +0200

    Delete header_parser.py

--- header_parser.py
+++
@@ -1,7 +0,0 @@
-# a help function to split the headers and transforming in a dictionary to be able to use when doing a request
-def headers_parser(raw_headers):
-	headers = {}
-	for line in raw_headers.split('\n'):
-		x = line.split(': ')
-		headers[x[0]] = x[1]
-	return headers
